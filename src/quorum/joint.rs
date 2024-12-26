5e528394fa6b746dfe02290207af82eddd002f6e
Hung Tran <hungtran511997@gmail.com>
2024-12-26 17:14:36 +0700

    feat: impl joint consensus

--- src/quorum/joint.rs
+++ src/quorum/joint.rs
@@ -1,6 +1,53 @@
+use core::fmt;
+use std::{
+    collections::{HashMap, HashSet},
+    fmt::{Debug, Display, Formatter},
+};
+
 use crate::MajorityConfig;
 
+/// Index is a Raft log position.
+#[derive(Default, Clone, Copy)]
+pub struct Index {
+    pub index: u64,
+    pub group_id: u64,
+}
 
+impl Display for Index {
+    #[inline]
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        match self.group_id {
+            0 => match self.index {
+                u64::MAX => write!(f, "∞"),
+                index => write!(f, "{}", index),
+            },
+            group_id => match self.index {
+                u64::MAX => write!(f, "[{}]∞", group_id),
+                index => write!(f, "[{}]{}", group_id, index),
+            },
+        }
+    }
+}
+
+impl Debug for Index {
+    #[inline]
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        Display::fmt(self, f)
+    }
+}
+
+pub trait AckedIndexer {
+    fn acked_index(&self, voter_id: u64) -> Option<Index>;
+}
+
+pub type AckIndexer = HashMap<u64, Index>;
+
+impl AckedIndexer for AckIndexer {
+    #[inline]
+    fn acked_index(&self, voter: u64) -> Option<Index> {
+        self.get(&voter).cloned()
+    }
+}
 /// A configuration of two groups of (possibly overlapping) majority configurations.
 /// Decisions require the support of both majorities.
 #[derive(Clone, Debug, Default, PartialEq, Eq)]
@@ -9,3 +56,12 @@ pub struct Configuration {
     pub(crate) outgoing: MajorityConfig,
 }
 
+impl Configuration {
+    /// Creates a new configuration using the given IDs.
+    pub fn new(voters: HashSet<u64>) -> Configuration {
+        Configuration {
+            incoming: MajorityConfig::new(voters),
+            outgoing: MajorityConfig::new(voters),
+        }
+    }
+}
--- src/quorum/majority.rs
+++ src/quorum/majority.rs
@@ -1,7 +1,29 @@
 use std::collections::HashSet;
+use std::fmt::Formatter;
 
 /// A set of IDs that uses majority quorums to make decisions.
 #[derive(Clone, Debug, Default, PartialEq, Eq)]
 pub struct Configuration {
     voters: HashSet<u64>,
+}
+
+impl std::fmt::Display for Configuration {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "({})",
+            self.voters
+                .iter()
+                .map(|x| x.to_string())
+                .collect::<Vec<String>>()
+                .join(" ")
+        )
+    }
+}
+
+impl Configuration {
+    /// Creates a new configuration using the given IDs.
+    pub fn new(voters: HashSet<u64>) -> Configuration {
+        Configuration { voters }
+    }
 }
